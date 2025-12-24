649387500fe3ad2cc15ade8656e2c819ce3da804
Ahmet Kilinc <akx9@icloud.com>
2025-12-24 20:24:12 +0000

    fix

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,35 +1,35 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const getPublicServerUrl = () => {
   if (import.meta.env.VITE_VERCEL_ENV === "production") {
-    return `https://${import.meta.env.VITE_VERCEL_PROJECT_PRODUCTION_URL}`
+    return `https://${import.meta.env.VITE_VERCEL_PROJECT_PRODUCTION_URL}`;
   } else if (import.meta.env.VITE_VERCEL_ENV === "preview") {
-    return `https://${import.meta.env.VITE_VERCEL_BRANCH_URL}`
+    return `https://${import.meta.env.VITE_VERCEL_BRANCH_URL}`;
   } else {
-    return `http://localhost:3000`
+    return `http://localhost:3000`;
   }
-}
+};
 
 export const getWorkerUrl = () => {
   if (import.meta.env.VITE_VERCEL_ENV === "production") {
-    return `https://${import.meta.env.VITE_API_URL}`
+    return `https://${import.meta.env.VITE_API_URL}`;
   } else if (import.meta.env.VITE_VERCEL_ENV === "preview") {
-    return `https://${import.meta.env.VITE_API_URL}`
+    return `https://${import.meta.env.VITE_API_URL}`;
   } else {
-    return `http://localhost:8787`
+    return `http://localhost:3001`;
   }
-}
+};
 
 export const getGitUrl = () => {
-  const workerUrl = getWorkerUrl()
+  const workerUrl = getWorkerUrl();
   if (workerUrl) {
-    return workerUrl
+    return workerUrl;
   }
-  const baseUrl = getPublicServerUrl()
-  return `${baseUrl}/api/git`
-}
+  const baseUrl = getPublicServerUrl();
+  return `${baseUrl}/api/git`;
+};
