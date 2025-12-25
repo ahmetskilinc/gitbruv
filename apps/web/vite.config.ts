f71e2174b657d8a1cff64274e0723c2a9db01977
Ahmet Kilinc <akx9@icloud.com>
2025-12-25 10:13:52 +0000

    test?

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -21,6 +22,7 @@ export default defineConfig({
         routesDirectory: "app",
       },
     }),
+    nitro({ preset: 'bun' })
     viteReact(),
   ],
 });
