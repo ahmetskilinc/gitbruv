c231d824f083768f57837cb3782177eee144ee51
Ahmet Kilinc <akx9@icloud.com>
2025-12-30 20:44:21 +0000

    this is so fucking stupid

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -6,12 +6,12 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getPublicServerUrl = () => {
-  console.log("PUBLIC SERVER URL", process.env.NODE_ENV, process.env.RAILWAY_PUBLIC_DOMAIN, import.meta.env.VITE_RAILWAY_PUBLIC_DOMAIN);
+  console.log("PUBLIC SERVER URL", process.env.NODE_ENV, process.env.RAILWAY_PUBLIC_DOMAIN, process.env.VITE_RAILWAY_PUBLIC_DOMAIN);
   if (process.env.NODE_ENV === "production") {
     if (process.env.RAILWAY_PUBLIC_DOMAIN) {
       return `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
     } else {
-      return `https://${import.meta.env.VITE_RAILWAY_PUBLIC_DOMAIN}`;
+      return `https://${process.env.VITE_RAILWAY_PUBLIC_DOMAIN}`;
     }
   } else {
     return `http://localhost:3000`;
@@ -19,12 +19,12 @@ export const getPublicServerUrl = () => {
 };
 
 export const getApiUrl = () => {
-  console.log("API URL", process.env.NODE_ENV, process.env.API_URL, import.meta.env.VITE_API_URL);
+  console.log("API URL", process.env.NODE_ENV, process.env.API_URL, process.env.VITE_API_URL);
   if (process.env.NODE_ENV === "production") {
     if (process.env.API_URL) {
       return `https://${process.env.API_URL}`;
     } else {
-      return `https://${import.meta.env.VITE_API_URL}`;
+      return `https://${process.env.VITE_API_URL}`;
     }
   } else {
     return `http://localhost:3001`;
